4545ee15af7290d8d6a7f026b625727f6869f969
DUC <87541853+pad1092@users.noreply.github.com>
2023-05-09 14:43:12 +0700

    Update VNPayConfig.java

--- src/main/java/com/duc/vnpaydemo/Config/VNPayConfig.java
+++ src/main/java/com/duc/vnpaydemo/Config/VNPayConfig.java
@@ -15,8 +15,8 @@ import java.util.*;
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
     public static String vnp_Returnurl = "/vnpay-payment";
-    public static String vnp_TmnCode = "DL98SYAD";
-    public static String vnp_HashSecret = "GSRPJZPYHIVOCDDHASPZQXITAYKCNFED";
+    public static String vnp_TmnCode = "";
+    public static String vnp_HashSecret = "";
     public static String vnp_apiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
 
     public static String md5(String message) {
